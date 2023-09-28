57aef55f14339b85bf4f40a1fc977d995b3477dd
Ibraheem Mobolaji Abdulsalam <ibmabdulsalam@gmail.com>
2023-09-28 06:12:05 +0100

    Update models.py

--- backend/bio/models.py
+++ backend/bio/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from user.models import User
-from rest_framework.serializers import ModelSerializer
 class Bio(models.Model):
     user = models.ForeignKey(User, related_name="user_bio", on_delete=models.CASCADE)
     body = models.TextField()
@@ -8,8 +7,3 @@ class Bio(models.Model):
 
     def __str__(self):
         return f"{self.created_on}"
-
-class BioSerializer(ModelSerializer):
-    class Meta:
-        model = Bio
-        fields = "__all__"
