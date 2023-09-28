4e751a2a72bc67dd2ffa2da72afcc8adda6fba20
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2023-09-28 12:53:56 +0100

    subscription and jwt auth added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,7 +65,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+ASGI_APPLICATION = 'backend.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
