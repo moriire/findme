1a3b1ce3c49c65729e3c50acaba4fa705ba97fdf
Ibraheem Mobolaji Abdulsalam <ibmabdulsalam@gmail.com>
2023-09-28 06:23:16 +0100

    Update views.py

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,6 +1,2 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
 from .models import User, UserSerializer
-class UserView(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
